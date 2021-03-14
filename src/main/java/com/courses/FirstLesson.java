57c18b2cdd82996c6c6ce7a5372f0c3df5637662
admin <niksakhatskiy@gmail.com>
2021-03-14 16:05:17 +0200

    Переделаное ДЗ 1 урок

--- src/main/java/com/courses/FirstLesson.java
+++ src/main/java/com/courses/FirstLesson.java
@@ -7,67 +7,59 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
+import java.time.Duration;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class FirstLesson {
     public static void main(String[] args) {
-        WebDriverManager.chromedriver().setup(); // - либо так
-        //System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe"); // путь где лежит вебдравер - либо так
+        WebDriverManager.chromedriver().setup();
 
 //1.Открыть сайт https://www.saucedemo.com
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.saucedemo.com"); // открытие сайта
+        driver.get("https://www.saucedemo.com");
 
-//2-3.Ввести логин, пароль и залогинится. Сделал как на уроке, через .getText()) не понял как, спросить у Жени
+//2-3.Ввести логин, пароль и залогинится.
 
-        /*WebElement loginText = driver.findElement(By.xpath("//div[@class='login_credentials']")); // находим блок в котором лежит текст с логинами
-        String login = loginText.getText(); // собираем весь текст который там лежит
-        System.out.println(login); // выводим текст в консоль, хз как выбрать один из вариантов(спросить у Жени)
+        WebElement usernameField = driver.findElement(By.id("user-name"));
+        WebElement loginText = driver.findElement(By.id("login_credentials"));
+        String login = loginText.getText();
+        String[] log = login.split("\n");
+        usernameField.sendKeys(log[1]);
 
-        WebElement passwordText = driver.findElement(By.xpath("//div[@class='login_password']")); // находим блок в котором лежит текст с паролем
-        String password = passwordText.getText(); // собираем весь текст который там лежит
-        System.out.println(password); // выводим текст в консоль, хз как выбрать один из вариантов(спросить у Жени)*/
-
-        // пока напишу как на уроке разбирали
-        WebElement usernameField = driver.findElement(By.className("form_input")); // поиск поля юзернейм по класу
-        usernameField.sendKeys("standard_user"); // ввод логина в поле юзернейм
-        WebElement passwordField = driver.findElement(By.xpath("//input[2]")); // поиск поля пасворд по икспасу
-        passwordField.sendKeys("secret_sauce"); // ввод пароля в поле пасворд
-        WebElement loginButton = driver.findElement(By.xpath("//input[@type='submit']")); // поиск кнопки логин по икспасу
-        loginButton.click(); // жмем кнопку логин
+        WebElement passwordField = driver.findElement(By.id("password"));
+        WebElement passwordText = driver.findElement(By.className("login_password"));
+        String password = passwordText.getText();
+        String[] pass = password.split("\n");
+        passwordField.sendKeys(pass[1]);
+        WebElement loginButton = driver.findElement(By.id("login-button"));
+        loginButton.click();
 
 // 4.Добавить в корзину товар под номером 2
-        List<WebElement> list = driver.findElements(By.xpath("//button")); // нахождение кнопки добавление
-        WebElement addToCart = list.get(3); // выбираем 2 кнопку
-        addToCart.click(); // жмем вторую кнопку
+        List<WebElement> list = driver.findElements(By.xpath("//button"));
+        WebElement addToCart = list.get(3);
+        addToCart.click();
 
 //5.Отсортировать товар в обратном порядке
-        WebElement filter = driver.findElement(By.className("product_sort_container")); // находми кнопку сортировки
+        WebElement filter = driver.findElement(By.className("product_sort_container"));
         Select select = new Select(filter);
-        List<WebElement> listOfOptions = select.getOptions();
-
-        for(int i = 0; i < listOfOptions.size(); i++) {
-            System.out.println(listOfOptions.get(i).getText());
-        }
-
-        int indexForReversionFilter = 1;
-        select.selectByIndex(indexForReversionFilter);
-        select.selectByValue("za");
         select.selectByVisibleText("Name (Z to A)");
 
 //6.Добавить в корзину товар под номером 3
-        List<WebElement> liSt = driver.findElements(By.xpath("//button")); // нахождение кнопки добавление
-        WebElement addToCarts = liSt.get(4); // выбираем 3 кнопку
-        addToCarts.click(); // жмем вторую кнопку
+        List<WebElement> newlist = driver.findElements(By.xpath("//button"));
+        WebElement addToCarts = newlist.get(4);
+        addToCarts.click();
 
 //7.Вылогинится с сайта
-        WebElement menu = driver.findElement(By.className("bm-burger-button")); //находим кнопку меню
-        menu.click(); // жмем на кнопку меню
+        WebElement menu = driver.findElement(By.className("bm-burger-button"));
+        menu.click();
 
-        WebElement logout = driver.findElement(By.xpath("(//a)[3]")); //находим кнопку меню
-        logout.click(); // жмем на кнопку логаут. Не работает выход чего-то
+        WebElement logout = driver.findElement(By.id("react-burger-menu-btn"));
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS); //неявное ожидание 1 секунда
+        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); //явное ожидание 1 секунда, но так чего-то не сработало
+        logout.click();
 
-        driver.quit();  //завершение теста, закрывается тестовое окно, если не писать сожрет весь ресурс пк, если тестов будет много*/
+        driver.quit();
 
     }
 }
