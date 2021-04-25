b471a47ac53784300363958bc4ac2ab7fa12fdf4
admin <niksakhatskiy@gmail.com>
2021-04-25 11:26:14 +0300

    Добавлены скрины при падении Добавлено логирование Добавлено аннотаций Step, Feature, Story, Listeners, Link

--- src/test/java/homework/LoginPositiveTest.java
+++ src/test/java/homework/LoginPositiveTest.java
@@ -1,5 +1,9 @@
 package homework;
 
+import io.qameta.allure.Feature;
+import io.qameta.allure.Issue;
+import io.qameta.allure.Link;
+import io.qameta.allure.Story;
 import org.apache.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
@@ -8,21 +12,23 @@ import pages.InventoryPage;
 import pages.LoginPage;
 import parentPackages.BeforeAfterMethods;
 
-
+@Feature("Test")
+@Story("Positive")
 public class LoginPositiveTest extends BeforeAfterMethods {
 
     @DataProvider(name = "valuesToEnter")
     private Object[][] valuesToEnter() {
         String password = "secret_sauce";
-        return new Object[][]{{"standard_user",password},{"problem_user",password},{"performance_glitch_user",password}};
+        return new Object[][]{{"standard_user",password},{"pblem_user",password},{"performance_glitch_user",password}};
     }
 
+    @Link(url = "https://google.com", name = "GOOGLE")
     @Test(dataProvider = "valuesToEnter")
     public void loginWithDifferentUserNames(String username, String password) {
         LoginPage loginPage = new LoginPage(driver);
-        loginPage.usernameField.sendKeys(username);
-        loginPage.passwordField.sendKeys(password);
-        loginPage.loginButton.click();
+        loginPage.setUsernameField(username);
+        loginPage.setPasswordField(password);
+        loginPage.setLoginButton();
         InventoryPage logoutAct = new InventoryPage(driver);
         String confirmingText = logoutAct.getConfirmingClass().getText();
         Assert.assertEquals(confirmingText,"PRODUCTS");
--- src/test/java/parentPackages/ParentClass.java
+++ src/test/java/parentPackages/ParentClass.java
@@ -1,5 +1,7 @@
 package parentPackages;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
@@ -8,7 +10,10 @@ public abstract class ParentClass {
 
     protected WebDriver driver;
 
+    Logger log = Logger.getLogger(this.getClass());
+
     protected ParentClass(WebDriver driver) {
+        PropertyConfigurator.configure("src/test/resources/properties/log4j.properties");
         this.driver = driver;
         PageFactory.initElements(driver,this);
     }
